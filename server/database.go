f93949248388f56a0e13ea441a786f71cd068f65
wnjuguna <wm453856@gmail.com>
2024-07-29 22:13:23 +0300

    feat: Database for login/signup page

---
+++ server/database.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+    "encoding/json"
+    "io/ioutil"
+    "os"
+)
+
+type User struct {
+    Username string `json:"username"`
+    Password string `json:"password"`
+}
+
+
+func readUsers(filePath string) ([]User, error) {
+    var users []User
+    file, err := os.Open(filePath)
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
+
+    data, err := ioutil.ReadAll(file)
+    if err != nil {
+        return nil, err
+    }
+
+    err = json.Unmarshal(data, &users)
+    if err != nil {
+        return nil, err
+    }
+
+    return users, nil
+}
+
+func writeUsers(filePath string, users []User) error {
+    data, err := json.MarshalIndent(users, "", "  ")
+    if err != nil {
+        return err
+    }
+
+    err = ioutil.WriteFile(filePath, data, 0644)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+
+
+func addUser(filePath string, newUser User) error {
+    users, err := readUsers(filePath)
+    if err != nil {
+        return err
+    }
+
+    users = append(users, newUser)
+    return writeUsers(filePath, users)
+}
