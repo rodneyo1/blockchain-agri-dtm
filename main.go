24d56e41034921f0a05c9b2eb071a738e7fad863
LuvDokta <clifford242526@gmail.com>
2024-07-29 20:38:42 +0300

    working server

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
-import
-(
+import (
 	"fmt"
+	"net/http"
+	"os"
+	"Bitcoin/server"
+	
 )
 
-func main(){
-	fmt.Println("This will be our initial")
+func main() {
+	if len(os.Args) != 1 {
+		return
+	}
+	fs := http.FileServer(http.Dir("static"))
+	// assets := http.FileServer(http.Dir("assets"))
+	http.HandleFunc("/", Bitcoin.Home)
+	// http.HandleFunc("/ascii-art", handler.Art)
+	http.Handle("/static/", http.StripPrefix("/assets/static/", fs))
+	// http.Handle("/assets/", http.StripPrefix("/assets/", assets))
+	// http.HandleFunc("/about", handler.About)
+	// http.HandleFunc("/instructions", handler.Instructions)
+	fmt.Println("Server is starting at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
--- server/server.go
+++ server/server.go
@@ -1,11 +1,11 @@
-package server
+package Bitcoin
 
 import (
-	"fmt"
+	// "fmt"
 	"net/http"
 	"text/template"
 
-	functions "server/functions"
+	// functions "server/functions"
 )
 
 type ascii struct {
@@ -14,7 +14,7 @@ type ascii struct {
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	home := template.Must(template.ParseFiles("./template/index.html"))
+	home := template.Must(template.ParseFiles("./web/templates/index.html"))
 	w.WriteHeader(http.StatusOK)
 
 	home.Execute(w, nil)
@@ -41,40 +41,40 @@ func ErrorPage(w http.ResponseWriter, statusCode int, message string) {
 	tmpl.Execute(w, data)
 }
 
-func Art(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		ErrorPage(w, http.StatusBadRequest, "400 - Bad Request")
-		return
-	}
+// func Art(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method != http.MethodPost {
+// 		ErrorPage(w, http.StatusBadRequest, "400 - Bad Request")
+// 		return
+// 	}
 
-	r.ParseForm()
-	text := r.FormValue("input")
-	banner := r.FormValue("bannerfile")
-	ascii_Art, err := functions.Input(text, banner)
-	fmt.Println(err)
+// 	r.ParseForm()
+// 	text := r.FormValue("input")
+// 	banner := r.FormValue("bannerfile")
+// 	// ascii_Art, err := functions.Input(text, banner)
+// 	fmt.Println(err)
 
-	if err != nil {
-		if err.Error() == "file not found" {
+// 	if err != nil {
+// 		if err.Error() == "file not found" {
 
-			// w.WriteHeader(http.StatusNotFound)
-			ErrorPage(w, http.StatusNotFound, "404 - Not Found")
-			return
-		}
-		if err.Error() == ("non ascii Character") {
-			ErrorPage(w, http.StatusInternalServerError, "400 - Bad Request")
-			return
-		}
+// 			// w.WriteHeader(http.StatusNotFound)
+// 			ErrorPage(w, http.StatusNotFound, "404 - Not Found")
+// 			return
+// 		}
+// 		if err.Error() == ("non ascii Character") {
+// 			ErrorPage(w, http.StatusInternalServerError, "400 - Bad Request")
+// 			return
+// 		}
 
-		ErrorPage(w, http.StatusInternalServerError, "500 - Internal Server Error")
-		return
-	}
+// 		ErrorPage(w, http.StatusInternalServerError, "500 - Internal Server Error")
+// 		return
+// 	}
 
-	// fmt.Println(ascii_Art)
-	data := ascii{AsciiArt: ascii_Art}
+// 	// fmt.Println(ascii_Art)
+// 	data := ascii{AsciiArt: ascii_Art}
 
-	home := template.Must(template.ParseFiles("template/index.html"))
-	err2 := home.Execute(w, data)
-	if err2 != nil {
-		fmt.Println(err2)
-	}
-}
+// 	home := template.Must(template.ParseFiles("template/index.html"))
+// 	err2 := home.Execute(w, data)
+// 	if err2 != nil {
+// 		fmt.Println(err2)
+// 	}
+// }
