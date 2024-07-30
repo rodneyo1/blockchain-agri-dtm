2325137b02bb82280a4723675d7c0e41865a2084
LuvDokta <clifford242526@gmail.com>
2024-07-30 16:10:00 +0300

    fix for bugs

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	Bitcoin "Bitcoin/server"
+)
+
+
+// type User struct {
+//     Username string `json:"username"`
+//     Password string `json:"password"`
+// }
+
+func main() {
+	if len(os.Args) != 1 {
+		return
+	}
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static")))) // fs := http.FileServer(http.Dir("static"))
+	// assets := http.FileServer(http.Dir("assets"))
+	http.HandleFunc("/home", Bitcoin.Home)
+	http.HandleFunc("/checkout", Bitcoin.Checkout)
+	http.HandleFunc("/login", Bitcoin.HandleLogin)
+	http.HandleFunc("/registeration", Bitcoin.HandlerRegisterPAge)
+	http.HandleFunc("/register", Bitcoin.HandleRegister)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/", "/login":
+			Bitcoin.Home(w, r)
+		default:
+			// Bitcoin.ErrorPage(w, http.StatusNotFound, "404 - Not Found")
+		}
+	})
+	fmt.Println("Server is running on port :http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func Serve(w http.ResponseWriter, r *http.Request) {
+	imagePath := "./" + r.URL.Path
+	_, err := os.Stat(imagePath)
+	if err != nil {
+		fmt.Fprintf(w, "Image not found")
+	}
+
+	content, _ := os.ReadFile(imagePath)
+	fmt.Fprintf(w, "%s", string(content))
+}
---
+++ server/database.go
@@ -0,0 +1,99 @@
+package Bitcoin
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+// type User struct {
+// 	Firstname string `json:"first"`
+// 	Lastname  string `json:"last"`
+// 	Email     string `json:"email"`
+// 	Username  string `json:"username"`
+// 	Password  string `json:"password"`
+// 	Location  string `json:"location"`
+// 	Contract  string `json:"mobile"`
+// }
+
+func ReadUsers(filePath string) ([]UserID, error) {
+	file, err := os.Open(filePath)
+	fmt.Println(err)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	defer file.Close()
+
+	var User []UserID
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&User)
+	if err != nil && err != io.EOF {
+		return nil, err
+	}
+	return User, nil
+}
+
+func WriteUsersToFile(filePath string, users []UserID) error {
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0o644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	return encoder.Encode(users)
+}
+
+// func RegisterUsers(filename string, newUser User) error {
+// 	users, err := readUsers(filename)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	// Check if user already exists
+// 	for _, user := range users {
+// 		if user.Username == newUser.Username {
+// 			return fmt.Errorf("user already exists")
+// 		}
+// 	}
+
+// 	// Hash the password before storing
+// 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	newUser.Password = string(hashedPassword)
+
+// 	// Append new user
+// 	users = append(users, newUser)
+// 	return WriteUsers(filename, users)
+// }
+
+func UserExists(users []UserID, username string) bool {
+	for _, user := range users {
+		if user.Username == username {
+			return true
+		}
+	}
+	return false
+}
+
+// func AuthenticateUser(filename, username, password string) (bool, error) {
+// 	users, err := readUsers(filename)
+// 	if err != nil {
+// 		return false, err
+// 	}
+
+// 	for _, user := range users {
+// 		if user.Username == username {
+// 			err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+// 			if err != nil {
+// 				return false, nil
+// 			}
+// 			return true, nil
+// 		}
+// 	}
+
+// 	return false, nil
+// }
